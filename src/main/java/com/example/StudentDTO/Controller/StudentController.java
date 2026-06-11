6b8fde3db8c8a7f89d2657305341609d84415721
abul <ahmadharish95@gmail.com>
2026-06-12 01:55:46 +0530

    Sl4l logs added

--- src/main/java/com/example/StudentDTO/Controller/StudentController.java
+++ src/main/java/com/example/StudentDTO/Controller/StudentController.java
@@ -6,10 +6,17 @@ import com.example.StudentDTO.Service.StudentServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+
+    //Logger Object
+    private static final Logger log = LoggerFactory.getLogger(StudentController.class);
 
     private final StudentServiceImpl service;
 
@@ -19,33 +26,45 @@ public class StudentController {
     }
 
 
-    //Create Student
+    //Create Student===================
 
     @PostMapping("/create")
     public StudentResponceDTO createStudent(@RequestBody StudentRequestDTO dto){
-        return service.createStudent(dto);
+
+        log.info("Post /Student API Called");            // Log when API is called
+
+        //return service.createStudent(dto);
+        StudentResponceDTO responceDTO = service.createStudent(dto);
+
+        log.info("Controller Student created Sucessfully with Id: {}",responceDTO.getId());    // Log after successful creation
+        return responceDTO;
     }
 
-    //Fetch All Student
+    //Fetch All Student=================
     @GetMapping("/getAll")
     public List<StudentResponceDTO> getAllStudent()
     {
+        log.info("GET All /getAll API Called");
 
         return service.getAll();
     }
 
-    //Fetch by Id
+    //Fetch by Id===================
     @GetMapping("/getid/{id}")
     public  StudentResponceDTO getById(@PathVariable Long id)
     {
+        log.info("GET by id  /getid API Called: {}",id);
         return service.getStudentById(id);
     }
 
 
-    //Delete by Id
+    //Delete by Id=======================
     @DeleteMapping("/delete/{id}")
     public String deleteStudent(@PathVariable Long id){
+
+        log.info("DELETE by id /delete/id: {}",id);
         service.deleteByid(id);
+
         return "Student Deleted Sucessfully";
     }
 
--- src/main/java/com/example/StudentDTO/Service/StudentServiceImpl.java
+++ src/main/java/com/example/StudentDTO/Service/StudentServiceImpl.java
@@ -5,19 +5,27 @@ import com.example.StudentDTO.DTO.StudentResponceDTO;
 import com.example.StudentDTO.Enitity.Student;
 import com.example.StudentDTO.Repository.StudentRepository;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
-
 @Data       // @Getter and @Setter
 public class StudentServiceImpl implements StudentServiceInterface {
 
-  private final StudentRepository repository;
 
-    public StudentServiceImpl(StudentRepository repository) {
+    //Loger Object
+    private static final Logger log  = LoggerFactory.getLogger(StudentServiceImpl.class);
+
+  private final StudentRepository repository;   // Value is final cannot be change
+
+    public StudentServiceImpl(StudentRepository repository) {   // Contructor Dependency Injection
         this.repository = repository;
     }
 
@@ -25,15 +33,21 @@ public class StudentServiceImpl implements StudentServiceInterface {
     @Override
     public StudentResponceDTO createStudent(StudentRequestDTO dto) {
 
-        //DTO-> Entity
+        log.info("Cretaing Student");
+
+        //Request DTO-> Entity
         Student student = new Student();
 
+
         student.setName(dto.getName());
+        log.info("Student Data recived", dto.getName());
         student.setEmail(dto.getEmail());
         student.setDepartment(dto.getDepartment());
         student.setSalary(dto.getSalary());
         student.setPassword(dto.getPassword());
+
         Student saveStudent = repository.save(student);
+        log.info("Student saved sucessfully. id: {}", saveStudent);
 
 
         // Entity -> Response DTO
@@ -50,9 +64,13 @@ public class StudentServiceImpl implements StudentServiceInterface {
     //Fetch All Student
     @Override
     public List<StudentResponceDTO> getAll() {
+
+        log.info("fatching all student from database");
         return repository.findAll()
                 .stream()                             //Stream is used to process collections
                 .map(student -> {              //Map Used to convert one object into another object.
+
+                    log.debug("Processing Student ID: {}",student.getId() );
                     StudentResponceDTO dto = new StudentResponceDTO();     // Create a new  dto object
 
                     dto.setId(student.getId());      //Gets ID from Student entity. and Set Id in DTO(Copy data from Entity to DTO and store in DTO)
@@ -70,11 +88,17 @@ public class StudentServiceImpl implements StudentServiceInterface {
     //Fetch by Id
     @Override
     public StudentResponceDTO getStudentById(Long id) {
+
+        log.info("Fetching Student by ID: {}",id);
+
         Student student = repository.findById(id)
                 .orElseThrow(()->new RuntimeException("Student Not Found"));
+                log.error("Student not Found witg Id: {}",id);
 
     StudentResponceDTO dto = new StudentResponceDTO();
     dto.setId(student.getId());
+
+    log.info("Student found: {}",student.getId());
     dto.setName(student.getName());
     dto.setEmail(student.getEmail());
     dto.setDepartment(student.getDepartment());
@@ -84,7 +108,16 @@ public class StudentServiceImpl implements StudentServiceInterface {
 
     @Override
     public void deleteByid(Long id) {
+        log.info("Deleteing Student with Id: {}", id);
         repository.deleteById(id);
+        log.info("Student deleted SucessFully with Id: {}",id);
 
     }
 }
+
+
+/*logger.info("Information message");
+logger.debug("Debug message");
+logger.warn("Warning message");
+logger.error("Error message");
+* */
